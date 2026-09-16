b2596bdbcb1f5f580c906b2af1b6452f766f76fd
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-16 11:45:26 +0900

    feat: export agent runtime primitives

---
+++ causalrag/agent/__init__.py
@@ -0,0 +1,12 @@
+from .actions import ActionKind, CandidateAction, DecisionRecord
+from .loop import CausalAgentLoop
+from .state import AgentState, Observation
+
+__all__ = [
+    "ActionKind",
+    "CandidateAction",
+    "DecisionRecord",
+    "CausalAgentLoop",
+    "AgentState",
+    "Observation",
+]
