fb3d03b55e0c70fff3788e1582e32a9da0e4fc76
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-17 00:13:08 +0900

    Export model mismatch and contract expansion APIs

--- causalrag/experiments/__init__.py
+++ causalrag/experiments/__init__.py
@@ -6,14 +6,23 @@ from .decision import (
     experiment_decision_value,
     intervention_value,
 )
+from .mismatch import (
+    ModelMismatchAssessment,
+    ModelMismatchPolicy,
+    assess_model_mismatch,
+    maybe_resolve_model_mismatch,
+)
 from .models import (
     ExperimentContract,
     ExperimentUpdate,
     OutcomeLikelihood,
     apply_experiment_observation,
     contract_applicable,
+    expanded_experiment_contract,
     expected_information_gain,
+    outcome_surprisal,
     posterior_for_outcome,
+    predictive_probability,
 )
 from .preferences import DecisionPreferences
 
@@ -25,10 +34,17 @@ __all__ = [
     "InterventionValue",
     "ExperimentDecisionValue",
     "DecisionPreferences",
+    "ModelMismatchPolicy",
+    "ModelMismatchAssessment",
     "contract_applicable",
     "expected_information_gain",
     "posterior_for_outcome",
+    "predictive_probability",
+    "outcome_surprisal",
+    "expanded_experiment_contract",
     "apply_experiment_observation",
+    "assess_model_mismatch",
+    "maybe_resolve_model_mismatch",
     "intervention_value",
     "best_intervention_value",
     "experiment_decision_value",
