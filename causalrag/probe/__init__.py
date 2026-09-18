c705d420461417fa3b6459135f06a19efb51000c
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-18 10:25:42 +0900

    Expose interactive probe sessions

--- causalrag/probe/__init__.py
+++ causalrag/probe/__init__.py
@@ -1,3 +1,10 @@
 from .runtime import ProbeRunConfig, available_probe_config, run_probe_episode
+from .session import InteractiveProbeSession, ProbePreview
 
-__all__ = ["ProbeRunConfig", "available_probe_config", "run_probe_episode"]
+__all__ = [
+    "ProbeRunConfig",
+    "available_probe_config",
+    "run_probe_episode",
+    "InteractiveProbeSession",
+    "ProbePreview",
+]
