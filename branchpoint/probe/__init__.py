87d96f5eccc29306bda37cbbf0c327ebe2b18e72
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-21 13:51:51 +0900

    api: expose capability-pack registration

--- branchpoint/probe/__init__.py
+++ branchpoint/probe/__init__.py
@@ -2,6 +2,13 @@ from .runtime import ProbeRunConfig, available_probe_config, build_probe_agent, 
 from .compare import run_probe_comparison
 from .ladder import capability_ladder_profiles, run_probe_ladder
 from .session import ProbeSession, ProbeSessionManager, SESSION_MANAGER, sse_stream
+from .scenarios import (
+    ProbeScenarioRuntime,
+    ProbeScenarioSpec,
+    get_probe_scenario,
+    register_probe_scenario,
+    unregister_probe_scenario,
+)
 
 __all__ = [
     "ProbeRunConfig",
@@ -15,4 +22,9 @@ __all__ = [
     "ProbeSessionManager",
     "SESSION_MANAGER",
     "sse_stream",
+    "ProbeScenarioRuntime",
+    "ProbeScenarioSpec",
+    "get_probe_scenario",
+    "register_probe_scenario",
+    "unregister_probe_scenario",
 ]
