513e49a79329046a378f9acb4c62c2317c581af9
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-22 16:45:11 +0900

    legal: align package classifier with Apache-2.0

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
