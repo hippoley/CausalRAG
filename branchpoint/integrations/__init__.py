4e85123dbea160581b11a6c2849c71be4da104cc
hippo <135493401+hippoley@users.noreply.github.com>
2026-09-23 15:38:55 +0900

    feat: add integrations package

---
+++ branchpoint/integrations/__init__.py
@@ -0,0 +1,13 @@
+from .openai_agents import (
+    ApprovalOutcome,
+    OpenAIAgentsApprovalAdapter,
+    OpenAIAgentsResolution,
+    OpenAIAgentsToolDecision,
+)
+
+__all__ = [
+    "ApprovalOutcome",
+    "OpenAIAgentsApprovalAdapter",
+    "OpenAIAgentsResolution",
+    "OpenAIAgentsToolDecision",
+]
